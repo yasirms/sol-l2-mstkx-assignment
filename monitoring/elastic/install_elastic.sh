062b5127ad27effc7b5267b8f7f2cdab5c8de9f9
Yasir Mehmood <yasirmehmood@Yasirs-MacBook-Pro.local>
2019-08-05 18:29:14 +0200

    updated all files

---
+++ monitoring/elastic/install_elastic.sh
@@ -0,0 +1,17 @@
+kubectl create namespace logging
+
+# Install Elastic Search
+kubectl create -f kubernetes/elastic.yaml -n logging
+
+
+#Install Kibana
+kubectl create -f kubernetes/kibana.yaml -n logging
+
+
+#Create Fluentd-ROle Base Access Control
+kubectl create -f kubernetes/fluentd-rbac.yaml
+
+
+# Create Fluentd Deployment
+kubectl create -f kubernetes/fluentd-daemonset.yaml
+
